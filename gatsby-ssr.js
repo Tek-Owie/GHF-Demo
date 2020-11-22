e7eafa05d6c0336e63569d1527bce3c1c7cf2051
Tek-Owie <eodickson14@gmail.com>
2020-11-22 18:47:04 +0100

    Added global styles

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,35 @@
+import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Theme from "./src/themes/theme";
+
+
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        background-color: ${props => props.theme.colors.light1};
+    }
+`
+
+export const wrapRootElement = ({ element }) => (
+    <ThemeProvider theme={Theme}>
+        <GlobalStyles />
+    {element}
+    </ThemeProvider>
+)
+
+
+
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ * Implement Gatsby's Browser APIs in this file.
  *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
+ * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
 // You can delete this file if you're not using it
